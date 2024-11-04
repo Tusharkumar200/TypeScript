a4f9a5f970145c5be552e41198d4f65debed55c1
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-04 19:14:21 +0530

    slice created

--- project/LangLearn/src/main.tsx
+++ project/LangLearn/src/main.tsx
@@ -4,6 +4,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { ThemeProvider } from '@emotion/react'
 import { createTheme, CssBaseline } from '@mui/material'
+import { Provider } from 'react-redux'
+import { store } from './redux/store.ts'
 
 
 const theme = createTheme({
@@ -17,7 +19,10 @@ createRoot(document.getElementById('root')!).render(
 
   <ThemeProvider theme={theme}>
     <CssBaseline />
+    <Provider store={store}>
+
     <App />
+    </Provider>
   </ThemeProvider>
   
 )
---
+++ project/LangLearn/src/redux/slices.ts
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const rootSlice = createSlice({
+    name:"root",
+    initialState:{},
+    reducers:{},
+})
+
+export default rootSlice.reducer;
