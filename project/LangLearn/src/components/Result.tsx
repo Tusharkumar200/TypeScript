d35e2cbaa82a78aa35a1e8d0c87787c2311f55fd
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-07 15:08:59 +0530

    Result page made

--- project/LangLearn/src/components/Result.tsx
+++ project/LangLearn/src/components/Result.tsx
@@ -1,8 +1,52 @@
+import { Button, Container, List, ListItem, Stack, Typography } from "@mui/material";
 
 
+const result = ["Lol", "Sample", "Ans"];
+const words =[
+  {
+    meaning: "Asdsa",
+  },
+  {
+    meaning: "Asdsdsfda",
+  },
+]
 const Result = () => {
+  const correctAns = 5;
+  const percentage = (correctAns / words.length) * 100;
+
   return (
-    <div>Result</div>
+    <Container maxWidth={'sm'}>
+      <Typography variant='h3' color={"primary"} m={'2rem 0'}>Result</Typography>
+      <Typography variant='h6' m={'1rem '}>You got {correctAns} out of {words.length}</Typography>
+
+      <Stack direction={"row"} justifyContent={"space-evenly"}> 
+        <Stack>
+          <Typography m={"1rem 0"} variant="h5">Your Ans </Typography>
+          <List>
+            {
+              result.map((i, idx)=>(
+                <ListItem key={idx}>{idx +1} - {i}</ListItem>)
+              )
+            }
+          </List>
+        </Stack> 
+
+        <Stack>
+          <Typography m={"1rem 0"} variant="h5">Correct Ans </Typography>
+          <List>
+            {
+              words.map((i, idx)=>(
+                <ListItem key={idx}>{idx +1} - {i.meaning}</ListItem>)
+              )
+            }
+          </List>
+        </Stack>
+        </Stack>
+
+        <Typography variant="h5" m={"1rem"} color={percentage > 50 ? "green": "red"}>You got {percentage > 50? "Pass": "Fail"}</Typography>
+
+        <Button sx={{margin: "1rem"}} variant="contained">Reset</Button>
+    </Container>
   )
 }
 
--- project/LangLearn/src/utils/features.ts
+++ project/LangLearn/src/utils/features.ts
@@ -1,5 +1,23 @@
 import axios from "axios";
 import { generate } from "random-words";
+import _ from "lodash";
+
+const generateMCQ = (
+  meaning:{
+  Text: string;
+},[], idx: number):string[]=>{
+
+  const correctAns:string = meaning[idx].Text;
+  // an array of all meanings except for the correct one
+  const allMeaningExceptForCorrect = meaning.filter(
+    (i)=>i.Text !== correctAns
+  );
+  // Randomly generate 3 incorrect options
+  const incorrectOptions: string[] = _.sampleSize(allMeaningExceptForCorrect,3).map((i)=>i.Text);
+
+  const mcqOptions = _.shuffle([correctAns, ...incorrectOptions]);
+  return mcqOptions;
+}
 
 export const translateWords = async (params:LangType): Promise<WordType[]> => {
   try {
@@ -24,7 +42,7 @@ export const translateWords = async (params:LangType): Promise<WordType[]> => {
 
     const receive: FetchedDataType[] = response.data;
     const arr: WordType[] = receive.map((i, idx) =>{
-
+      const options: string[] = generateMCQ(words, idx);
       return{
         word: i.translations[0].text,
         mening: words[idx].Text,
